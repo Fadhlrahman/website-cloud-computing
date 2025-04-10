f19bdd1e6967e7b52296d0adbbe11e94e6e2f9a0
Fadhlrahman <fitraarsil19@gmail.com>
2025-04-10 12:05:57 +0700

    menambahkan button pada menu home dan memberi efek pada tampilan ui

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,26 +2,27 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="min-h-screen overflow-x-hidden bg-belakang flex flex-col items-center px-6 py-12 ">
-      <h2 className="text-3xl font-bold text-element mb-10 text-center">
-        Pengertian Cryptocurrency
-      </h2>
-
-      <div className="text-justify max-w-7xl mx-auto space-y-10 ">
+    <div className="bg-belakang px-6 py-12">
+      <div className="text-justify max-w-7xl mx-auto space-y-10">
         {/* Pengertian */}
-        <p className="text-lg text-element leading-relaxed">
-          Cryptocurrency, atau mata uang kripto, adalah bentuk mata uang digital
-          yang menggunakan teknologi kriptografi untuk mengamankan transaksi,
-          mengontrol penciptaan unit-unit baru, dan memverifikasi transfer aset.
-          Cryptocurrency bersifat desentralisasi, yang berarti tidak
-          dikendalikan oleh otoritas pusat seperti bank atau pemerintah,
-          melainkan dijalankan oleh jaringan komputer secara global melalui
-          teknologi yang disebut blockchain.
-        </p>
+        <div className="hover:-translate-y-1 transform transition duration-300">
+          <h2 className="text-2xl font-bold text-element mb-4 max-w-7xl mx-auto">
+            Pengertian Cryptocurrency
+          </h2>
+          <p className="text-lg text-element leading-relaxed">
+            Cryptocurrency, atau mata uang kripto, adalah bentuk mata uang
+            digital yang menggunakan teknologi kriptografi untuk mengamankan
+            transaksi, mengontrol penciptaan unit-unit baru, dan memverifikasi
+            transfer aset. Cryptocurrency bersifat desentralisasi, yang berarti
+            tidak dikendalikan oleh otoritas pusat seperti bank atau pemerintah,
+            melainkan dijalankan oleh jaringan komputer secara global melalui
+            teknologi yang disebut blockchain.
+          </p>
+        </div>
 
         {/* Teknologi Blockchain */}
-        <div>
-          <h3 className="text-2xl font-bold text-element mb-4 text-center">
+        <div className="hover:-translate-y-1 transform transition duration-300">
+          <h3 className="text-2xl font-bold text-element mb-4 max-w-7xl mx-auto">
             Teknologi Blockchain
           </h3>
           <p className="text-lg text-element leading-relaxed">
@@ -36,8 +37,8 @@ export default function About() {
         </div>
 
         {/* Fungsi dan Manfaat */}
-        <div>
-          <h4 className="text-2xl font-bold text-element mb-4 text-center">
+        <div className="hover:-translate-y-1 transform transition duration-300">
+          <h4 className="text-2xl font-bold text-element mb-4 max-w-7xl mx-auto">
             Fungsi dan Manfaat Cryptocurrency
           </h4>
           <ul className="list-disc list-inside space-y-2 text-lg text-element leading-relaxed">
@@ -57,6 +58,64 @@ export default function About() {
               Mendukung teknologi aplikasi terdesentralisasi seperti smart
               contract, DeFi (Decentralized Finance), dan NFT (Non-Fungible
               Token).
+            </li>
+          </ul>
+        </div>
+
+        {/* kelebihan cryptocurrency */}
+        <div className="hover:-translate-y-1 transform transition duration-300">
+          <h4 className="text-2xl font-bold text-element mb-4 max-w-7xl mx-auto">
+            Kelebihan Cryptocurrency
+          </h4>
+          <ul className="list-disc list-inside space-y-2 text-lg text-element leading-relaxed">
+            <li>Desentralisasi: Tidak dikendalikan oleh pihak ketiga.</li>
+            <li>
+              Keamanan tinggi: Berkat teknologi kriptografi dan blockchain.
+            </li>
+            <li>
+              Transparansi: Semua transaksi tercatat dan dapat dilihat publik.
+            </li>
+            <li>
+              Efisiensi waktu dan biaya: Transaksi dapat dilakukan dengan cepat
+              dan biaya rendah.
+            </li>
+            <li>
+              Akses global: Siapa saja dapat menggunakannya tanpa batasan
+              geografis.
+            </li>
+          </ul>
+        </div>
+
+        {/* status cryptocurrency */}
+        <div className="hover:-translate-y-1 transform transition duration-300">
+          <h3 className="text-2xl font-bold text-element mb-4 max-w-7xl mx-auto">
+            Status Cryptocurrency di Indonesia
+          </h3>
+          <p className="text-lg text-element leading-relaxed">
+            Di Indonesia, cryptocurrency diakui sebagai aset komoditas digital
+            dan diatur oleh BAPPEBTI (Badan Pengawas Perdagangan Berjangka
+            Komoditi). Artinya, masyarakat dapat membeli dan menjual
+            cryptocurrency sebagai investasi. Namun, cryptocurrency tidak diakui
+            sebagai alat pembayaran yang sah.
+          </p>
+        </div>
+
+        {/* langkah awal */}
+        <div className="hover:-translate-y-1 transform transition duration-300">
+          <h4 className="text-2xl font-bold text-element mb-4 max-w-7xl mx-auto">
+            Langkah Awal Memulai Investasi Cryptocurrency
+          </h4>
+          <ul className="list-disc list-inside space-y-2 text-lg text-element leading-relaxed">
+            <li>
+              Pilih platform exchange terpercaya seperti Indodax atau
+              Tokocrypto.
+            </li>
+            <li>Lakukan verifikasi identitas (KYC).</li>
+            <li>Deposit dana dalam bentuk Rupiah.</li>
+            <li>Pilih mata uang kripto yang ingin dibeli.</li>
+            <li>
+              Simpan aset kripto di dompet digital yang aman (hot wallet atau
+              cold wallet).
             </li>
           </ul>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
       <div className="container mx-auto p-8 bg-element shadow-lg rounded-2xl mt-10">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {/* Text Content */}
-          <div className="flex flex-col items-center justify-center p-6 bg-element2 text-element rounded-lg shadow-md">
-            <h2 className="text-3xl font-bold mb-4">WELCOME TO OUR SITE</h2>
+          <div className="flex flex-col items-center justify-center p-6 bg-element2 text-element rounded-lg shadow-md hover:shadow-2xl hover:-translate-y-1 transform transition duration-300">
+            <h2 className="text-6xl font-bold mb-4 font-serif">CRYPTOZONE</h2>
             <p className="text-lg text-center max-w-md">
-              Lets See The Crypto for Yout Assets.
+              "Lets See The Crypto for Yout Assets"
             </p>
           </div>
 
-          {/* Image */}   
-          <div className="flex items-center justify-center">
+          {/* Image */}
+          <div className="flex items-center justify-center  hover:-translate-y-1 transform transition duration-300">
             <Image
               src="/images/gambar-koin.jpeg"
               width={400}
@@ -29,14 +29,20 @@ export default function Home() {
       </div>
 
       {/* Additional Information */}
-      <div className="container mx-auto mt-10 p-6 bg-element2 rounded-lg shadow-md text-center max-w-2xl">
+      <div className="container mx-auto mt-10 p-6 bg-element2 rounded-lg shadow-md text-center max-w-4xl hover:shadow-2xl hover:-translate-y-1 transform transition duration-300">
         <p className="text-lg text-element leading-relaxed">
-          Our website is dedicated to providing top-notch services and
-          innovative solutions. We believe in quality, excellence, and customer
-          satisfaction. Join us on this journey to explore new opportunities and
-          grow together!
+          Cryptozone merupakan salah satu platform yang dapat membantu anda
+          memantau mata uang kripto yang ingin anda beli. Selain itu Cryptozone
+          juga memberikan edukasi untuk anda yang baru memulai pada dunia
+          kriptografi.
         </p>
       </div>
+
+      <Link href="/servis" passHref>
+        <button className="mt-6 px-8 py-4 bg-green-700 text-white text-xl rounded-xl shadow hover:bg-green-800 transition">
+          Mulai Sekarang
+        </button>
+      </Link>
     </div>
   );
 }
